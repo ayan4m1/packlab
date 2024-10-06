0ae305829ba13ee4f0d606bd6139a9c6e38c97e8
ayan4m1 <andrew@bulletlogic.com>
2024-10-05 23:53:43 -0400

    implement basic init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,5 +1,109 @@
+import { valid } from 'semver';
+import { basename } from 'path';
+import { existsSync } from 'fs';
+import { input, checkbox } from '@inquirer/prompts';
+
+import {
+  getPackFilePath,
+  ModLoaders,
+  ModSources,
+  PackFile,
+  MinecraftVersions,
+  writePackFile,
+  MinecraftVersionType
+} from '../utils.js';
+
 try {
-  console.log('Hello World');
+  if (existsSync(getPackFilePath())) {
+    console.error(
+      'A packlab.yml already exists in this directory. Please ensure you are in the right place.'
+    );
+    process.exit(0);
+  }
+
+  const dirName = basename(process.cwd());
+  const name = await input({
+    message: 'What is the modpack name?',
+    required: true,
+    default: dirName
+  });
+  const author = await input({
+    message: 'Who is the modpack author?'
+  });
+  const description = await input({
+    message: 'Enter a short description, if desired.'
+  });
+  const url = await input({
+    message: 'Enter a URL, if available.'
+  });
+
+  const versionReq = await fetch(
+    'https://launchermeta.mojang.com/mc/game/version_manifest.json'
+  );
+  const versionData = (await versionReq.json()) as unknown as MinecraftVersions;
+
+  const versions = versionData.versions
+    .filter(
+      (version) =>
+        version.type === MinecraftVersionType.Release && valid(version.id)
+    )
+    .map((version) => version.id);
+
+  let supportedVersions = [];
+
+  while (!supportedVersions.length) {
+    supportedVersions = await checkbox<string>({
+      message: 'Which game versions should be supported?',
+      choices: versions
+    });
+
+    if (!supportedVersions.length) {
+      console.warn('Must select at least one game version!');
+    }
+  }
+
+  let sources = [];
+
+  while (!sources.length) {
+    sources = await checkbox<string>({
+      message: 'Which mod sources should be used?',
+      choices: Object.values(ModSources).map((sourceName: string) => ({
+        name: sourceName,
+        value: sourceName,
+        checked: sourceName === ModSources.Modrinth
+      }))
+    });
+
+    if (!sources.length) {
+      console.warn('Must select at least one mod source!');
+    }
+  }
+
+  let loaders = [];
+
+  while (!loaders.length) {
+    loaders = await checkbox<string>({
+      message: 'Which mod loaders should be supported?',
+      choices: Object.values(ModLoaders)
+    });
+
+    if (!loaders.length) {
+      console.warn('Must select at least one mod loader!');
+    }
+  }
+
+  const newPackFile: PackFile = {
+    name,
+    author,
+    description,
+    url,
+    sources,
+    versions,
+    loaders,
+    mods: []
+  };
+
+  await writePackFile(process.cwd(), newPackFile);
 } catch (error) {
   console.error(error);
   process.exit(1);
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,63 @@
 import type { PackageJson } from '@npmcli/package-json';
 
+import { parse, stringify } from 'yaml';
+import { satisfies, validRange } from 'semver';
 import { fileURLToPath } from 'url';
+import { readFile, writeFile } from 'fs/promises';
 import { dirname, resolve } from 'path';
 import { PathLike, existsSync } from 'fs';
 import packageJsonModule from '@npmcli/package-json';
 
+export enum ModSources {
+  Modrinth = 'modrinth',
+  CurseForge = 'curseforge'
+}
+
+export enum ModLoaders {
+  Forge = 'forge',
+  Fabric = 'fabric',
+  NeoForged = 'neoforged'
+}
+
+export enum MinecraftVersionType {
+  Snapshot = 'snapshot',
+  Release = 'release'
+}
+
+export type MinecraftVersion = {
+  id: string;
+  type: MinecraftVersionType;
+  url: string;
+  time: Date;
+  releaseTime: Date;
+};
+
+export type MinecraftVersions = {
+  latest: {
+    release: string;
+    snapshot: string;
+  };
+  versions: MinecraftVersion[];
+};
+
+export type PackFile = {
+  name: string;
+  author?: string;
+  description?: string;
+  url?: string;
+  sources: string[];
+  loaders: string[];
+  versions: string[];
+  mods: string[];
+};
+
 type ExecOptions = {
   executableFile: string;
+};
+
+type VersionSpecifier = {
+  id: string;
+  version: string;
 };
 
 const modpackFilePath = 'modpack.yml';
@@ -14,10 +65,8 @@ const modpackFilePath = 'modpack.yml';
 export const getInstallDirectory = (): string =>
   dirname(fileURLToPath(import.meta.url));
 
-const getPackageJsonPath = (): string => resolve(getInstallDirectory(), '..');
-
 export const getPackageInfo = async (): Promise<PackageJson> =>
-  (await packageJsonModule.load(getPackageJsonPath()))?.content;
+  (await packageJsonModule.load(resolve(getInstallDirectory(), '..')))?.content;
 
 export const isModpackDirectory = (dir: PathLike = process.cwd()) =>
   existsSync(resolve(dir.toString('utf-8'), modpackFilePath));
@@ -25,3 +74,63 @@ export const isModpackDirectory = (dir: PathLike = process.cwd()) =>
 export const getExecOptions = (...paths: string[]): ExecOptions => ({
   executableFile: `lib/commands/${paths.join('/')}`
 });
+
+export const getPackFilePath = (dir: string = process.cwd()) =>
+  resolve(dir, 'packlab.yml');
+
+export const readPackFile = async (dir?: string): Promise<PackFile> => {
+  const path = getPackFilePath(dir);
+
+  if (!existsSync(path)) {
+    return null;
+  }
+
+  const contents = await readFile(path, 'utf-8');
+
+  if (!contents.length) {
+    return null;
+  }
+
+  return parse(contents) as unknown as PackFile;
+};
+
+export const writePackFile = async (
+  dir: string,
+  data: PackFile
+): Promise<void> => {
+  const path = getPackFilePath(dir);
+
+  if (existsSync(path)) {
+    console.warn(`Asked to overwrite packlab.yml in ${dir}`);
+    return;
+  }
+
+  await writeFile(path, stringify(data), 'utf-8');
+};
+
+export const parseVersionString = (input: string): VersionSpecifier | null => {
+  if (!input.includes('@')) {
+    input += '@*';
+  }
+
+  const tokens = input.split('@');
+
+  if (tokens.length !== 2) {
+    console.warn(`Version specifier had multiple @ symbols: ${input}`);
+    return null;
+  }
+
+  const [id, version] = tokens;
+
+  if (!validRange(version)) {
+    console.warn(`Version range is invalid: ${version}`);
+    return null;
+  }
+
+  return { id, version };
+};
+
+export const findMatchingVersions = (
+  expression: string,
+  versions: VersionSpecifier[]
+) => versions.filter(({ version }) => satisfies(version, expression));
